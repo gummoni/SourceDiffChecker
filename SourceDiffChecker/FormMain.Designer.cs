c18afc31ca40354d8a988e6c4181d5481e947ef9
test <sat_stc@mbn.nifty.com>
2016-09-28 13:08:12 +0900

    プロジェクト ファイルを追加します。

---
+++ SourceDiffChecker/FormMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SourceDiffChecker
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btClose = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btFolderSetting = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btHashCompare = new System.Windows.Forms.Button();
+            this.btHashCreate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btClose);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btFolderSetting);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btHashCompare);
+            this.panel1.Controls.Add(this.btHashCreate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 73);
+            this.panel1.TabIndex = 0;
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(582, 10);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 40);
+            this.btClose.TabIndex = 7;
+            this.btClose.Text = "終了";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "*.vb";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ハッシュ化するファイル拡張子";
+            // 
+            // btFolderSetting
+            // 
+            this.btFolderSetting.Location = new System.Drawing.Point(338, 4);
+            this.btFolderSetting.Name = "btFolderSetting";
+            this.btFolderSetting.Size = new System.Drawing.Size(31, 23);
+            this.btFolderSetting.TabIndex = 4;
+            this.btFolderSetting.Text = "...";
+            this.btFolderSetting.UseVisualStyleBackColor = true;
+            this.btFolderSetting.Click += new System.EventHandler(this.btFolderSetting_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "プロジェクトフォルダ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(223, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btHashCompare
+            // 
+            this.btHashCompare.Location = new System.Drawing.Point(490, 10);
+            this.btHashCompare.Name = "btHashCompare";
+            this.btHashCompare.Size = new System.Drawing.Size(75, 40);
+            this.btHashCompare.TabIndex = 1;
+            this.btHashCompare.Text = "ハッシュ比較";
+            this.btHashCompare.UseVisualStyleBackColor = true;
+            this.btHashCompare.Click += new System.EventHandler(this.btHashCompare_Click);
+            // 
+            // btHashCreate
+            // 
+            this.btHashCreate.Location = new System.Drawing.Point(399, 10);
+            this.btHashCreate.Name = "btHashCreate";
+            this.btHashCreate.Size = new System.Drawing.Size(75, 40);
+            this.btHashCreate.TabIndex = 0;
+            this.btHashCreate.Text = "ハッシュ作成";
+            this.btHashCreate.UseVisualStyleBackColor = true;
+            this.btHashCreate.Click += new System.EventHandler(this.btHashCreate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "BuntyuMae.sln";
+            this.openFileDialog1.Filter = "slnファイル|*.sln|すべてのファイル|*.*";
+            this.openFileDialog1.InitialDirectory = "c:\\";
+            this.openFileDialog1.Title = "BuntyuMae.slnを指定してください";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox3.Location = new System.Drawing.Point(0, 73);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(717, 478);
+            this.textBox3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 551);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ソース比較ツール";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btFolderSetting;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btHashCompare;
+        private System.Windows.Forms.Button btHashCreate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ SourceDiffChecker/FormMain.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SourceDiffChecker
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            //コンストラクタ
+            InitializeComponent();
+
+            var dir1 = Folder.SearchFile(@"c:\Buntyu", "BuntyuMae.sln");
+            var dir2 = Folder.SearchFile(@"c:\project\srl\mae", "BuntyuMae.sln");
+
+            if (string.Empty != dir1)
+            {
+                textBox1.Text = dir1;
+                textBox1.BackColor = Color.Lime;
+            }
+            else if (string.Empty != dir2)
+            {
+                textBox1.Text = dir2;
+                textBox1.BackColor = Color.Lime;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Yellow;
+            }
+        }
+
+        private void btFolderSetting_Click(object sender, EventArgs e)
+        {
+            //プロジェクトファイルを探すダイアログ表示
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var result = openFileDialog1.FileName;
+                textBox1.Text = Path.GetDirectoryName(result);
+                textBox1.BackColor = Color.Lime;
+            }
+        }
+
+        void LogWrite(string message)
+        {
+            textBox3.Invoke(new Action(() =>
+            {
+                textBox3.Text += message + "\r\n";
+                textBox3.SelectionLength = textBox3.Text.Length;
+                textBox3.ScrollToCaret();
+            }));
+        }
+
+        private async void btHashCreate_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                LogWrite("ハッシュ作成開始");
+                //ハッシュリスト作成
+                var dir = textBox1.Text;
+                var ext = textBox2.Text;
+                var collection = HashCollection.Create(dir, ext);
+                collection.Save("hashlist.txt");
+                LogWrite("ハッシュ作成完了");
+            });
+        }
+
+        private async void btHashCompare_Click(object sender, EventArgs e)
+        {
+            //ハッシュリスト比較
+            await Task.Run(() =>
+            {
+                LogWrite("ハッシュ比較開始");
+                var dir = textBox1.Text;
+                var ext = textBox2.Text;
+                var collection1 = HashCollection.Load("hashlist.txt");  //比較先（見本）
+                var collection2 = HashCollection.Create(dir, ext);      //比較元
+                var errorFileList = new List<string>();
+
+                //比較
+                foreach (var model in collection2)
+                {
+                    if (!collection1.Compare(model))
+                    {
+                        if (0 > model.Filename.IndexOf(".g.i.vb"))
+                        {
+                            LogWrite($"不一致: {model.Filename}");
+                            errorFileList.Add(model.Fullname);
+                        }
+                    }
+                }
+
+                //結果
+                if (0 == errorFileList.Count)
+                {
+                    LogWrite("完全一致！");
+                }
+                else
+                {
+                    //不一致あり
+                    var machineName = Environment.MachineName;
+                    foreach (var ch in Path.GetInvalidFileNameChars())
+                    {
+                        machineName = machineName.Replace(ch, '_');
+                    }
+                    LogWrite($"{errorFileList.Count}件 不一致項目がありました。");
+                    LogWrite($"USBメモリ内に[{machineName}_src.zip]ファイルを作成するので、メールで送ってください");
+                    Files.Compress(errorFileList.ToArray(), $"{machineName}_src.zip", dir);
+                }
+            });
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            //閉じる
+            Close();
+        }
+    }
+
+
+    /// <summary>
+    /// フォルダヘルパー
+    /// </summary>
+    public static class Folder
+    {
+        /// <summary>
+        /// 指定したファイルがどこにあるか探す
+        /// </summary>
+        /// <param name="startDirectory">検索開始フォルダ</param>
+        /// <param name="filename">探すファイル名</param>
+        /// <returns>ディレクトリ（見つからない場合はEmptyを返す）</returns>
+        public static string SearchFile(string startDirectory, string filename)
+        {
+            var ext = "*" + Path.GetExtension(filename);
+            var files = Directory.GetFiles(startDirectory, ext, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var dirname = Path.GetDirectoryName(file);
+                var fname = Path.GetFileName(file);
+                if (filename == fname)
+                {
+                    return dirname;
+                }
+            }
+            return string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// ファイルヘルパー
+    /// </summary>
+    public static class Files
+    {
+        /// <summary>
+        /// ファイルらをZIP圧縮
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="zipFilename"></param>
+        public static void Compress(string[] files, string zipFilename, string offsetPath)
+        {
+            if (File.Exists(zipFilename))
+            {
+                File.Delete(zipFilename);
+            }
+            using (var z = ZipFile.Open(zipFilename, ZipArchiveMode.Create))
+            {
+                foreach (var file in files)
+                {
+                    z.CreateEntryFromFile(file, file.Replace(offsetPath, "."), CompressionLevel.Optimal);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// ファイルハッシュ
+    /// </summary>
+    public class HashModel
+    {
+        public string Fullname { get; set; }
+        public string Filename { get; set; }
+        public string Hash { get; set; }
+        public string Update { get; set; }
+
+        /// <summary>
+        /// 文字列化
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => $"{Filename},{Hash},{Update}";
+
+        /// <summary>
+        /// 文字列からモデル生成
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public static HashModel Parse(string line)
+        {
+            var rows = line.Split(',');
+            if (rows.Length == 3)
+            {
+                return new HashModel { Filename = rows[0], Hash = rows[1], Update = rows[2] };
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public HashModel()
+        {
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// 引数で指定したファイルのハッシュを計算してHash値を代入する
+        /// </summary>
+        /// <param name="filename"></param>
+        public HashModel(string filename, string offsetDir)
+        {
+            Fullname = filename;
+            Filename = filename.Replace(offsetDir, "");
+            using (var md5 = MD5.Create())
+            using (var stream = File.OpenRead(filename))
+            {
+                var bytes = md5.ComputeHash(stream);
+                Hash = string.Join("", bytes.Select(_=> _.ToString("X2")));
+                Update = File.GetLastWriteTime(filename).ToString();
+            }
+        }
+    }
+
+    /// <summary>
+    /// ハッシュリスト
+    /// </summary>
+    public class HashCollection : List<HashModel>
+    {
+        /// <summary>
+        /// ハッシュリストの保存
+        /// </summary>
+        public void Save(string filename)
+        {
+            var lines = this.Select(_ => _.ToString());
+            File.WriteAllLines(filename, lines);
+        }
+
+        /// <summary>
+        /// ハッシュリストの呼出
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static HashCollection Load(string filename)
+        {
+            var collection = new HashCollection();
+            if (File.Exists(filename))
+            {
+                var lines = File.ReadAllLines(filename, Encoding.UTF8);
+                foreach (var line in lines)
+                {
+                    var model = HashModel.Parse(line);
+                    collection.Add(model);
+                }
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// ファイルハッシュリストを作成する
+        /// </summary>
+        /// <param name="directoryName">検索するディレクトリ名</param>
+        /// <param name="ext">検索する拡張子</param>
+        /// <returns></returns>
+        public static HashCollection Create(string directoryName, string ext)
+        {
+            var collection = new HashCollection();
+            var files = Directory.GetFiles(directoryName, ext, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                collection.Add(new HashModel(file, directoryName));
+            }
+            return collection;
+        }
+
+        /// <summary>
+        /// ハッシュ比較
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool Compare(HashModel model)
+        {
+            var _model = this.FirstOrDefault(_ => _.Filename == model.Filename);
+            if (null == _model)
+            {
+                return true;
+            }
+
+            if (_model.Hash == model.Hash)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
